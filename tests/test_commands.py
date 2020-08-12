7f55ae3fa4e40523f0fffe9ea0e1460d12dd199c
Antoine Augusti <antoine.augusti@gmail.com>
2020-08-12 16:36:55 -0400

    Basic implementation

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -13,61 +13,185 @@ def datasets():
 
 
 @pytest.fixture
+def mock_invalid_response():
+    return [
+        {"foo": "bar"}
+    ]
+
+
+@pytest.fixture
 def mock_response(datasets):
     ds1, ds2, ds3 = datasets
     return [
         {
-            # invalid ID, should not crash the command
+            # Invalid ID, but valid reco: should not crash the command
             "id": "1",
             "recommendations": [
                 {
-                    "id": str(ds1.id)
+                    "id": str(ds1.id),
+                    "score": 50
                 }
             ]
         },
         {
-            # valid ID and recos, expect for third element
+            # valid ID and recos,
             # should process two elements w/o crashing
+            # should reorder by score and handle reco by ID and slug
             "id": str(ds2.id),
             "recommendations": [
                 {
-                    "id": str(ds1.id)
+                    "id": str(ds3.id),
+                    "score": 1
                 },
                 {
-                    "id": str(ds3.id)
+                    "id": str(ds1.slug),
+                    "score": 2
                 },
                 {
-                    "id": "%s1234" % str(ds3.id)
+                    "id": "nope",
+                    "score": 50
                 }
             ]
         },
         {
-            # empty recos, should not crash the command
+            # Valid ID but recommended dataset does not exist
             "id": str(ds3.id),
-            "recommendations": []
-        },
-        {
-            # invalid schema, should not crash the command
-            "dataset": "xxx"
+            "recommendations": [
+                {
+                    "id": "nope",
+                    "score": 50
+                },
+            ]
         }
     ]
 
 
-@pytest.mark.options(RECOMMENDATIONS_DATASETS_SOURCE_URL=MOCK_URL)
-def test_dataset_reco_command(cli, rmock, mock_response, datasets):
-    ds1, ds2, ds3 = datasets
-    ds4 = DatasetFactory(extras={'recommendations': ['xxx']})
-    rmock.get(MOCK_URL, json=mock_response)
-    result = cli('recommendations datasets')
-    assert 'Dataset recommendations filled for 1 dataset' in result.output
-    # previous recommendations have been cleaned up
-    ds4.reload()
-    assert ds4.extras == {}
-    # correct recommendations have been filled
+@pytest.mark.usefixtures('clean_db')
+def test_clean_source(cli, datasets):
+    _ = datasets
+
+    ds = DatasetFactory(extras={
+        'recommendations:sources': ['foo', 'bar'],
+        'recommendations': [
+            {
+                'id': 'id1',
+                'source': 'bar',
+                'score': 50
+            },
+            {
+                'id': 'id2',
+                'source': 'foo',
+                'score': 50
+            },
+        ]
+    })
+    ds2 = DatasetFactory(extras={
+        'recommendations:sources': ['foo'],
+        'recommendations': [
+            {
+                'id': 'id2',
+                'source': 'foo',
+                'score': 50
+            },
+        ]
+    })
+
+    result = cli('recommendations clean foo')
+    assert 'Cleaned up 2 dataset(s)' in result.output
+
+    ds.reload()
+    assert ds.extras == {
+        'recommendations:sources': ['bar'],
+        'recommendations': [
+            {
+                'id': 'id1',
+                'source': 'bar',
+                'score': 50
+            }
+        ]
+    }
     ds2.reload()
-    assert ds2.extras['recommendations'] == [str(ds1.id), str(ds3.id)]
-    # wrong recommendations have not been filled
+    assert ds2.extras == {}
+
+
+def test_datasets_recommendations_wrong_args(cli):
+    result = cli('recommendations datasets -u https://example.com', check=False)
+    assert result.exit_code == -1
+    assert "You should specify a source and a URL" in result.output
+
+    result = cli('recommendations datasets -s fake_source', check=False)
+    assert result.exit_code == -1
+    assert "You should specify a source and a URL" in result.output
+
+
+def test_datasets_recommendations_invalid_data(cli, mock_invalid_response, rmock):
+    rmock.get(MOCK_URL, json=mock_invalid_response)
+
+    result = cli(f'recommendations datasets -s fake_source -u {MOCK_URL}', check=False)
+
+    assert result.exit_code == -1
+    assert result.exit_code == -1
+    assert "Fetched data is invalid" in result.output
+
+
+@pytest.mark.options(RECOMMENDATIONS_DATASETS={'fake_source': MOCK_URL})
+def test_datasets_recommendations_invalid_data_in_config(cli, mock_invalid_response, rmock):
+    rmock.get(MOCK_URL, json=mock_invalid_response)
+
+    result = cli(f'recommendations datasets')
+    assert result.exit_code == 0  # Command did not crash but outputs an error
+    assert "Fetched data is invalid" in result.output
+
+
+@pytest.mark.options(RECOMMENDATIONS_DATASETS={'fake_source': MOCK_URL})
+def test_datasets_recommendations_from_config_empty_db(cli, rmock, mock_response, datasets):
+    ds1, ds2, ds3 = datasets
+    rmock.get(MOCK_URL, json=mock_response)
+
+    result = cli('recommendations datasets')
+
+    # Correct recommendations have been filled
+    ds2.reload()
+    assert ds2.extras['recommendations:sources'] == ['fake_source']
+    assert ds2.extras['recommendations'] == [
+        {'id': str(ds1.id), 'source': 'fake_source', 'score': 2},
+        {'id': str(ds3.id), 'source': 'fake_source', 'score': 1},
+    ]
+
+    # Invalid recommendations have not been filled
     ds1.reload()
     ds3.reload()
     assert ds1.extras == {}
     assert ds3.extras == {}
+
+
+@pytest.mark.options(RECOMMENDATIONS_DATASETS={'fake_source': MOCK_URL})
+def test_datasets_recommendations_from_config_(cli, rmock, mock_response, datasets):
+    ds1, ds2, ds3 = datasets
+    ds4 = DatasetFactory()
+    rmock.get(MOCK_URL, json=mock_response)
+    ds2.extras['recommendations:sources'] = ['existing']
+    ds2.extras['recommendations'] = [
+        {'id': str(ds4.id), 'source': 'existing', 'score': 50},
+    ]
+    ds2.save()
+
+    cli('recommendations datasets')
+
+    # Recommendations have been merged, new source has been added
+    ds2.reload()
+    assert set(ds2.extras['recommendations:sources']) == set(['existing', 'fake_source'])
+    assert ds2.extras['recommendations'] == [
+        {'id': str(ds4.id), 'source': 'existing', 'score': 50},
+        {'id': str(ds1.id), 'source': 'fake_source', 'score': 2},
+        {'id': str(ds3.id), 'source': 'fake_source', 'score': 1},
+    ]
+
+    # Clean recommendations from the `existing` source
+    cli('recommendations clean existing')
+    ds2.reload()
+    assert ds2.extras['recommendations:sources'] == ['fake_source']
+    assert ds2.extras['recommendations'] == [
+        {'id': str(ds1.id), 'source': 'fake_source', 'score': 2},
+        {'id': str(ds3.id), 'source': 'fake_source', 'score': 1},
+    ]
--- udata_recommendations/commands.py
+++ udata_recommendations/commands.py
@@ -1,8 +1,11 @@
 import logging
+import json
+from pathlib import Path
 
 import click
 import mongoengine
 import requests
+import jsonschema
 
 from flask import current_app
 
@@ -11,6 +14,8 @@ from udata.core.dataset.models import Dataset
 
 log = logging.getLogger(__name__)
 
+SCHEMA_FILENAME = 'schema.json'
+
 
 @cli.group()
 def recommendations():
@@ -18,29 +23,47 @@ def recommendations():
     pass
 
 
-def clean_datasets_recommendations():
-    log.info('Cleaning up dataset recommendations...')
+def clean_datasets_recommendations(source):
+    log.info(f'Cleaning up dataset recommendations from source {source}...')
+
     datasets = Dataset.objects.filter(**{
-        'extras__recommendations__exists': True,
-    }).update(**{
-        'unset__extras__recommendations': True,
+        'extras__recommendations:sources__contains': source,
     })
-    success('Cleaned up %s dataset(s).' % datasets)
+
+    for dataset in datasets:
+        new_sources = set(
+            [s for s in dataset.extras['recommendations:sources'] if s != source]
+        )
+
+        # Dataset had only this source: delete all recommendations
+        if len(new_sources) == 0:
+            dataset.update(**{
+                'unset__extras__recommendations': True,
+                'unset__extras__recommendations:sources': True,
+            })
+            continue
+
+        # Dataset had at least one other source, keep only other sources
+        new_recommendations = [
+            r for r in dataset.extras['recommendations']
+            if r['source'] != source
+        ]
+        dataset.extras['recommendations:sources'] = list(new_sources)
+        dataset.extras['recommendations'] = new_recommendations
+        dataset.save()
+
+    success(f"Cleaned up {len(datasets)} dataset(s)")
 
 
-def get_datasets_data(url):
-    try:
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        exit_with_error('ConnectionError when trying to reach source.')
-    if not r.status_code == requests.codes.ok:
-        exit_with_error('Bad response from source (%s).' % r.status_code)
-    try:
-        data = r.json()
-    except ValueError:
-        exit_with_error('Bad response from source, expecting valid JSON.')
-    if not isinstance(data, list):
-        exit_with_error('Bad response from source, expecting JSON list.')
+def get_recommendations_data(url):
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+
+    with open(Path(__file__).parent / SCHEMA_FILENAME) as f:
+        schema = json.load(f)
+    jsonschema.validate(instance=data, schema=schema)
+
     return data
 
 
@@ -49,52 +72,73 @@ def get_dataset(id_or_slug):
     return obj or Dataset.objects.get(id=id_or_slug)
 
 
-def process_dataset(idx, dataset):
-    if 'id' not in dataset or 'recommendations' not in dataset:
-        error(' '.join(('Bad response from source for item #%s,',
-                        'expecting id and recommendations attributes.'))
-              % idx)
-        return 0
+def process_source(source, recommendations_data):
+    for dataset in recommendations_data:
+        process_dataset(source, dataset)
+
+
+def process_dataset(source, dataset):
     try:
-        dataset_obj = get_dataset(dataset['id'])
+        target_dataset = get_dataset(dataset['id'])
     except (Dataset.DoesNotExist, mongoengine.errors.ValidationError):
-        error('Dataset %s not found' % dataset['id'])
-        return 0
-    log.info('Processing recommendations for dataset %s.' % dataset['id'])
+        error(f"Dataset {dataset['id']} not found")
+        return
+
+    log.info(f"Processing recommendations for dataset {dataset['id']}")
     valid_recos = []
     for reco in dataset['recommendations']:
         try:
             reco_dataset_obj = get_dataset(reco['id'])
-            valid_recos.append(str(reco_dataset_obj.id))
+            valid_recos.append({
+                'id': str(reco_dataset_obj.id),
+                'score': reco['score'],
+                'source': source,
+            })
         except (Dataset.DoesNotExist, mongoengine.errors.ValidationError):
-            error('Recommended dataset %s not found' % reco['id'])
+            error(f"Recommended dataset {reco['id']} not found")
             continue
     if len(valid_recos):
-        success('Found %s recommendations for dataset %s.' % (
-                 len(valid_recos), dataset['id']))
-        dataset_obj.extras['recommendations'] = valid_recos
-        dataset_obj.save()
+        success(f"Found {len(valid_recos)} new recommendations for dataset {dataset['id']}")
+
+        new_sources = set(target_dataset.extras.get('recommendations:sources', []))
+        new_sources.add(source)
+
+        merged_recommendations = target_dataset.extras.get('recommendations', [])
+        merged_recommendations.extend(valid_recos)
+        new_recommendations = sorted(merged_recommendations, key=lambda k: k['score'], reverse=True)
+
+        target_dataset.extras['recommendations:sources'] = list(new_sources)
+        target_dataset.extras['recommendations'] = new_recommendations
+        target_dataset.save()
     else:
-        error('No recommendation found for dataset %s' % dataset['id'])
-        return 0
-    return 1
+        error(f"No recommendation found for dataset {dataset['id']}")
 
 
 @recommendations.command()
-@click.option('-s', '--source', default=None,
-              help='The source URL to get recommendations from')
-@click.option('-c', '--clean', is_flag=True, default=True,
-              help='Cleanup existing recommendations')
-def datasets(source, clean):
-    if clean:
-        clean_datasets_recommendations()
-    nb_datasets = 0
-    url = source or current_app.config.get(
-        'RECOMMENDATIONS_DATASETS_SOURCE_URL')
-    if not url:
-        exit_with_error('Not source url set, aborting.')
-    log.info('Fetching dataset recommendations from %s...', url)
-    data = get_datasets_data(url)
-    for idx, dataset in enumerate(data):
-        nb_datasets += process_dataset(idx, dataset)
-    success('Dataset recommendations filled for %s dataset(s).' % nb_datasets)
+@click.argument('source')
+def clean(source):
+    clean_datasets_recommendations(source)
+
+
+@recommendations.command()
+@click.option('-u', '--url', default=None, help='The URL to get recommendations from')
+@click.option('-s', '--source', default=None, help='The source name associated with this URL')
+def datasets(url, source):
+    use_config = url is None and source is None
+    if use_config:
+        for source, url in current_app.config.get('RECOMMENDATIONS_DATASETS').items():
+            log.info(f'Fetching dataset recommendations from {url}, source {source}')
+            try:
+                process_source(source, get_recommendations_data(url))
+            except jsonschema.exceptions.ValidationError as e:
+                error(f"Fetched data is invalid {str(e)}")
+        return
+
+    if url is None or source is None:
+        exit_with_error('You should specify a source and a URL.')
+
+    try:
+        log.info(f'Fetching dataset recommendations from {url}, source {source}')
+        process_source(source, get_recommendations_data(url))
+    except jsonschema.exceptions.ValidationError as e:
+        exit_with_error(f"Fetched data is invalid {str(e)}")
