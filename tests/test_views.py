b10791bd76b1491dc6d8629f02a25092b1d02e0a
Alexandre Bulté <alexandre.bulte@data.gouv.fr>
2020-11-13 15:28:27 +0100

    Add reuses support (#153)

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@ import pytest
 from flask import render_template_string, g
 
 from udata.core.dataset.factories import DatasetFactory
+from udata.core.reuse.factories import ReuseFactory
 
 
 def render_hook(dataset):
@@ -17,6 +18,11 @@ def datasets():
     return DatasetFactory.create_batch(3)
 
 
+@pytest.fixture
+def reuses():
+    return ReuseFactory.create_batch(1)
+
+
 @pytest.mark.frontend
 @pytest.mark.usefixtures('clean_db')
 @pytest.mark.usefixtures('app')
@@ -28,14 +34,18 @@ class TestViews:
         assert '' == render_hook(dataset=DatasetFactory(extras={}))
 
     @pytest.mark.options(RECOMMENDATIONS_NB_RECOMMENDATIONS=2)
-    def test_view_dataset_with_recommendations(self, datasets):
+    def test_view_dataset_with_recommendations(self, datasets, reuses):
         g.lang_code = 'fr'
         ds1, ds2, ds3 = datasets
+        r1 = reuses[0]
         dataset = DatasetFactory(extras={
             'recommendations': [
                 {'id': str(ds1.id), 'score': 100, 'source': 'dummy'},
                 {'id': str(ds2.id), 'score': 50, 'source': 'dummy'},
                 {'id': str(ds3.id), 'score': 25, 'source': 'dummy'},
+            ],
+            'recommendations-reuses': [
+                {'id': str(r1.id), 'score': 100, 'source': 'dummy'},
             ]
         })
 
@@ -44,22 +54,29 @@ class TestViews:
         assert ds1.full_title in content
         assert ds2.full_title in content
         assert ds3.full_title not in content
+        assert r1.title in content
 
     @pytest.mark.options(RECOMMENDATIONS_NB_RECOMMENDATIONS=2)
-    def test_view_dataset_with_recommendations_dedupe(self, datasets):
+    def test_view_dataset_with_recommendations_dedupe(self, datasets, reuses):
         g.lang_code = 'fr'
         ds1, ds2, ds3 = datasets
+        r1 = reuses[0]
         dataset = DatasetFactory(extras={
             'recommendations': [
                 {'id': str(ds1.id), 'score': 100, 'source': 'dummy'},
                 {'id': str(ds1.id), 'score': 50, 'source': 'other'},  # Duplicate ID
                 {'id': str(ds3.id), 'score': 25, 'source': 'dummy'},
+            ],
+            'recommendations-reuses': [
+                {'id': str(r1.id), 'score': 100, 'source': 'dummy'},
+                {'id': str(r1.id), 'score': 50, 'source': 'other'},
             ]
         })
 
         content = render_hook(dataset=dataset)
 
         assert content.count(ds1.full_title) == 1
+        assert content.count(f'<h4>{r1.title}</h4>') == 1
         assert ds1.full_title in content
         assert ds2.full_title not in content
         assert ds3.full_title in content
@@ -84,6 +101,10 @@ class TestViews:
 
         dataset = DatasetFactory(extras={
             'recommendations': [
+                {'id': str(ds1.id), 'score': 100, 'source': 'dummy'},
+            ],
+            'recommendations-reuses': [
+                # we don't care if the id is a dataset or a reuse here
                 {'id': str(ds1.id), 'score': 100, 'source': 'dummy'},
             ]
         })
--- udata_recommendations/views.py
+++ udata_recommendations/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, current_app
 from udata import theme
 
 from udata.core.dataset.models import Dataset
+from udata.core.reuse.models import Reuse
 from udata.frontend import template_hook
 
 blueprint = Blueprint('recommendations', __name__, template_folder='templates')
@@ -10,13 +11,16 @@ blueprint = Blueprint('recommendations', __name__, template_folder='templates')
 
 def has_recommendations(ctx):
     dataset = ctx['dataset']
-    return dataset and dataset.extras.get('recommendations', [])
+    return dataset and (
+        dataset.extras.get('recommendations', []) or dataset.extras.get('recommendations-reuses', [])
+    )
 
 
 @template_hook('dataset.display.after-description', when=has_recommendations)
 def dataset_recommendations(ctx):
     reco_ids = []
-    recommendations = ctx['dataset'].extras['recommendations']
+    recommendations = ctx['dataset'].extras.get('recommendations', [])
+    recommendations_reuses = ctx['dataset'].extras.get('recommendations-reuses', [])
 
     # Get at most n unique recommendations
     # Recommendations are already sorted by score in desc order
@@ -24,8 +28,10 @@ def dataset_recommendations(ctx):
     # Ordered sets don't exist but dicts are ordered since 3.7
     # https://stackoverflow.com/a/51145737
     reco_ids = list({r['id']: 0 for r in recommendations})[:limit]
+    reco_reuses_ids = list({r['id']: 0 for r in recommendations_reuses})[:limit]
 
     return theme.render(
         'dataset-recommendations.html',
-        recommendations=Dataset.objects.filter(id__in=reco_ids)
+        reco_datasets=Dataset.objects.filter(id__in=reco_ids),
+        reco_reuses=Reuse.objects.filter(id__in=reco_reuses_ids),
     )
